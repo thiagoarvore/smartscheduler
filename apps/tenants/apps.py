e7ec546dfc0a58b1b4575ef90973bd6853352115
Thiago Azevedo <thiagoarvore@gmail.com>
2026-06-05 07:04:31 +0000

    chore: reset codebase to docs and static assets only

--- apps/tenants/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class TenantsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.tenants"
-    verbose_name = "Tenants"
